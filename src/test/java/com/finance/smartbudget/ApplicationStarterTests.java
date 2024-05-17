ddc23a10203ea754d9d8f32b1dad1da540cd1b81
fiodor_gridiushko <grid467@gmail.com>
2024-05-17 19:26:04 +0300

    Проинициализирована базовая структура проекта Зависимости: lombok, spring(web,jpa,security), postgres driver,liquibase

---
+++ src/test/java/com/finance/smartbudget/ApplicationStarterTests.java
@@ -0,0 +1,13 @@
+package com.finance.smartbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
