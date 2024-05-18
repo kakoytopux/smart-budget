48ec58283e8a909de2947f3f8d873185c5cfad12
Джамал <Jonipux@yandex.ru>
2024-05-18 03:44:53 +0300

    добавлен liquibase

--- docker/init.sql
+++ docker/init.sql
@@ -1,6 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS smart_budget;
-
-CREATE TABLE IF NOT EXISTS smart_budget.users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -9,7 +7,7 @@ CREATE TABLE IF NOT EXISTS smart_budget.users (
     updated_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS smart_budget.transactions (
+CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY,
     transaction_sum DECIMAL,
     category VARCHAR(255),
--- src/main/resources/liquibase/migrations/V0.0.1_create_table.sql
+++ src/main/resources/liquibase/migrations/V0.0.1_create_table.sql
@@ -1,9 +1,7 @@
 --liquibase formatted sql
 --changeset puxichhh:1
 
-CREATE SCHEMA IF NOT EXISTS smart_budget;
-
-CREATE TABLE IF NOT EXISTS smart_budget.users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -12,7 +10,7 @@ CREATE TABLE IF NOT EXISTS smart_budget.users (
     updated_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS smart_budget.transactions (
+CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY,
     transaction_sum DECIMAL,
     category VARCHAR(255),
